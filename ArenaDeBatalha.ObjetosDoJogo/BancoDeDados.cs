16ed7ba3921fd98518ec1751bc70ebbfd6908739
DiegoSF01 <diegosouzafurlanetto@gmail.com>
2024-10-29 16:01:19 -0300

    v1

--- ArenaDeBatalha.ObjetosDoJogo/BancoDeDados.cs
+++ ArenaDeBatalha.ObjetosDoJogo/BancoDeDados.cs
@@ -1,27 +1,88 @@
-﻿using MySql.Data.MySqlClient;
-using System;
-using System.Data.SqlClient;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.Crmf;
 
-public class BancoDeDados
+namespace _231465
 {
-    private string connectionString = "Server=localhost;Database=ranking_jogo;Uid=root;Pwd=etecjau;Port=3307;";
-
-    // Método para abrir a conexão
-    public MySqlConnection AbrirConexao()
+    public class Banco
     {
-        MySqlConnection conexao = new MySqlConnection(connectionString);
-        conexao.Open();
-        return conexao;
-    }
+        //criando variaveis para conexao e consulta serão usadas em todo o projeto
+        //connection responsavel pela conexao com o MySQl
+        public static MySqlConnection Conexao;
+        // Command  responsavel pelas instrucoes sql a serem executadas
+        public static MySqlCommand Comando;
+        // adapter responsavel por inserir dados em um datatable
+        public static MySqlDataAdapter Adaptador;
+        public static DataTable datTabela;
 
-    // Método para fechar a conexão (boas práticas)
-    public void FecharConexao(MySqlConnection conexao)
-    {
-        if (conexao != null && conexao.State == System.Data.ConnectionState.Open)
+        public static void AbrirConexao()
         {
-            conexao.Close();
+            try
+            {
+                Conexao = new MySqlConnection("server=localhost;port=3307;uid=root;pwd=etecjau");
+
+                //Abre a conexao com o banco de dados
+                Conexao.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                // fecha a conexao com o banco de dados
+                Conexao.Close();
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                AbrirConexao();
+                //cria o banco
+                Comando = new MySqlCommand("CREATE DATABASE IF NOT EXISTS vendas; USE vendas", Conexao);
+                //executa a query no mysql (raio do workbench)
+                Comando.ExecuteNonQuery();
+
+                //cria atabela
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS Cidades " +
+                                            "(id integer auto_increment primary key, " +
+                                            "nome char(40), " +
+                                            "uf char(02))", Conexao);
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS Marcas " +
+                                            "(id integer auto_increment primary key, " +
+                                            "marca char(20))", Conexao);
+                Comando.ExecuteNonQuery();
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS Categorias " +
+                                            "(id integer auto_increment primary key, " +
+                                            "categoria char(20))", Conexao);
+                Comando.ExecuteNonQuery();
+
+                //chama a funcao para fechar a conexao com o banco
+                FecharConexao();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
-
